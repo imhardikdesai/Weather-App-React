3ce04b19c471bce426ad01d55ba25db743a83a83
imhardikdesai <hp676913@gmail.com>
2022-09-05 15:12:16 +0530

    Edit navbar and only place search bar

--- src/Components/MainContext/MainContext.jsx
+++ src/Components/MainContext/MainContext.jsx
@@ -4,10 +4,15 @@ import WeatherInfo from '../WeatherDataInfo/WeatherInfo'
 import './MainContext.css'
 
 export default function MainContext() {
+  fetch("https://api.openweathermap.org/data/2.5/weather?q=amreli&appid=fe4feefa8543e06d4f3c66d92c61b69c")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
   return (
     <div id='mainContext' className='container-fluid'>
-      <TimeBox/>
-      <WeatherInfo/>
+      <TimeBox />
+      <WeatherInfo />
     </div>
   )
 }
--- src/Components/WeatherDataInfo/WeatherInfo.jsx
+++ src/Components/WeatherDataInfo/WeatherInfo.jsx
@@ -3,13 +3,13 @@ import './Weatherinfo.css'
 export default function WeatherInfo() {
     return (
         <div id='weatherInfo'>
-            <img style={{filter:"invert()"}} src="https://static.thenounproject.com/png/3541181-200.png" alt="cloud" />
+            <img style={{ filter: "invert()" }} src="https://static.thenounproject.com/png/3541181-200.png" alt="cloud" />
             <h1 className='display-4'>Haze</h1><br />
             <h4>Delhi, IN</h4>
-            <p class="fs-4">Temperature &nbsp;&nbsp; 20°C Haze </p>
-            <p class="fs-4">Humidity &nbsp;&nbsp; 20% </p>
-            <p class="fs-4">Wind Speed &nbsp;&nbsp; 20km/h </p>
-            <p class="fs-4">Visibility &nbsp;&nbsp; 3000 mi </p>
+            <p className="fs-4">Temperature &nbsp;&nbsp; 20°C Haze </p>
+            <p className="fs-4">Humidity &nbsp;&nbsp; 20% </p>
+            <p className="fs-4">Wind Speed &nbsp;&nbsp; 20km/h </p>
+            <p className="fs-4">Visibility &nbsp;&nbsp; 3000 mi </p>
         </div>
     )
 }
