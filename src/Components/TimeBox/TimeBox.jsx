428956638ae5356c3b6d4872dc017f4427fcb694
Desai Hardik <87645745+imhardikdesai@users.noreply.github.com>
2022-10-28 17:13:58 +0530

    added live current date & time

--- src/Components/TimeBox/TimeBox.jsx
+++ src/Components/TimeBox/TimeBox.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './TimeBox.css'
 export default function TimeBox(props) {
+    const getCurrentTime = () => {
+        const date = new Date();
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        const seconds = date.getSeconds();
+        return `${hours}:${minutes}:${seconds}`;
+    }
+    const getCurrentDate = () => {
+        const date = new Date();
+        return date.toDateString();
+    }
+    const [date, setDate] = useState(getCurrentDate());
+    const [time, setTime] = useState(getCurrentTime());
+    setInterval(() => {
+        setTime(getCurrentTime());
+        setDate(getCurrentDate());
+    });
+
     return (
         <div id='timeBox' className='container'>
             <div className="clock">
-                <h1 className='display-5'>01:59:23</h1>
-                <h1 className='display-6'>Tuesday, 19 january 2022</h1>
+                <h1 className='display-5'>{time}</h1>
+                <h1 className='display-6'>{date}</h1>
             </div>
-            <div className="tempreature">
+            <div className="temperature">
                 <h1 className="display-1">{props.temperature}°C</h1>
             </div>
         </div>
